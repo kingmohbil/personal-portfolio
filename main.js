94a9cbadc601ffb7ebfa6a05efe862ed407d56c1
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-13 23:22:51 +0300

    Added on scroll transition

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
 const navbarBtn = document.querySelector('#arrow-btn');
 const navbar = document.querySelector('.nav-links');
 const arrowIcon = document.querySelector('.arrow');
+const hiddenElements = document.querySelectorAll('.hidden');
 
 navbarBtn.addEventListener('click', () => {
   navbarBtn.classList.toggle('active');
   navbar.classList.toggle('active');
 });
+
+hiddenElements.forEach((element) => observer.observe(element));
