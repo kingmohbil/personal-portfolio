761cd43d9db52ea532399860f442efa6761c6a9b
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-03-13 16:58:40 +0300

    Editing some classes in file

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navbarBtn = document.querySelector('#arrow-btn');
 const navbar = document.querySelector('.nav-links');
 const arrowIcon = document.querySelector('.arrow');
 
-navbarBtn.addEventListener('click', (e) => {
-  arrowIcon.classList.toggle('active');
+navbarBtn.addEventListener('click', () => {
+  navbarBtn.classList.toggle('active');
   navbar.classList.toggle('active');
 });
